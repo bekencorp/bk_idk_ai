b0682cbd3a3a12452cbd472af47357815509ff1b
itester <itester@aclsemi.com>
2025-03-01 23:49:37 +0800

    [CI] [52915]  <platform> <perf>Gsensor add enter low power configuration

--- components/bk_gsensor/gsensor_sc7a20.c
+++ components/bk_gsensor/gsensor_sc7a20.c
@@ -611,6 +611,7 @@ static int sc7a20_open(void)
         sc7a20_i2c_write(SL_SC7A20_CTRL_REG4,0x98);
         sc7a20_i2c_write(SL_SC7A20_CTRL_REG5,0x00);
         sc7a20_i2c_write(SL_SC7A20_CTRL_REG6,0x02);
+        sc7a20_i2c_write(SL_SC7A20_SDOI2C_PU_CFG,0xc);
         sc7a20_i2c_write(SL_SC7A20_INT1_CFG,0x2a);
         sc7a20_i2c_write(SL_SC7A20_INT1_THS,0x01);
         sc7a20_i2c_write(SL_SC7A20_INT1_DURATION,0x01);
