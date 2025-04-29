7c79b510cac481a3c7a59694ae8af8e50f5dae25
itester <itester@aclsemi.com>
2025-04-29 14:57:15 +0800

    [CI] [57044]  <platform> <fix> fix the issue of pvRealloc

--- components/os_source/freertos_v10/portable/MemMang/heap_4.c
+++ components/os_source/freertos_v10/portable/MemMang/heap_4.c
@@ -1644,7 +1644,11 @@ size_t xPortPointerSize(void * pv)
             configASSERT((pxLink->xBlockSize & xBlockAllocatedBit) != 0);
             configASSERT(pxLink->pxNextFreeBlock == NULL);
 
+            #if CONFIG_MEM_DEBUG
+            sz = pxLink->wantedSize;
+            #else
             sz = (pxLink->xBlockSize & ~xBlockAllocatedBit) - xHeapStructSize;
+            #endif
         }
         (void) xTaskResumeAll();
     }
