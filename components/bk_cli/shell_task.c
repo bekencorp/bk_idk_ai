0bf6ac52d8d3d4cece5ad7e580e5ca6b5f82ba31
itester <itester@aclsemi.com>
2025-04-29 14:57:24 +0800

    [CI] [57122]  <platform> <fix> adjust cpu1 dynamic log as psram buffer.

--- components/bk_cli/shell_task.c
+++ components/bk_cli/shell_task.c
@@ -229,12 +229,13 @@ struct dynamic_log_node_t
 	u8 ptr[0];
 };
 
-#if CONFIG_SYS_CPU0 && CONFIG_PSRAM_AS_SYS_MEMORY
+#if CONFIG_PSRAM_AS_SYS_MEMORY
 #define LOG_MALLOC psram_malloc
+#define LOG_FREE psram_free
 #else
 #define LOG_MALLOC os_malloc
-#endif
 #define LOG_FREE os_free
+#endif
 
 static dynamic_log_node s_dynamic_header = {NULL};
 static dynamic_log_node *s_to_free_list = NULL;
