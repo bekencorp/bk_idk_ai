0a86af7f0a88be7e549cb756ef6fda14ccdd61eb
itester <itester@aclsemi.com>
2025-05-23 21:39:34 +0800

    [CI] [58909]  <pm> <feat>Add dump cpu freq interface

--- components/bk_ps/include/bk_pm_internal_api.h
+++ components/bk_ps/include/bk_pm_internal_api.h
@@ -50,6 +50,17 @@ void pm_printf_current_temperature(void);
 
 uint64_t pm_cp1_aon_rtc_counter_get();
 
+bk_err_t bk_pm_cpu_freq_dump();
+
+#if CONFIG_PM_LV_TIME_COST_DEBUG
+__attribute__((section(".iram")))  uint64_t pm_lv_rtc_interval_get(pm_lv_step_e step);
+__attribute__((section(".iram")))  uint64_t pm_lv_rtc_tick_get(pm_lv_step_e step);
+__attribute__((section(".itcm_sec_code")))  bk_err_t pm_lv_rtc_tick_set(pm_lv_step_e step,uint64_t tick);
+__attribute__((section(".iram")))  bk_err_t pm_enter_lv_rtc_tick_clear();
+__attribute__((section(".iram")))  bk_err_t pm_wakeup_lv_rtc_tick_clear();
+__attribute__((section(".iram")))  uint64_t pm_rtc_cur_tick_get();
+#endif
+
 #ifdef __cplusplus
 }
 #endif
